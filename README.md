cfce34d913836463e5f503ebd8a6ff8c47729d4e
ajuwon adedapo <dapseen@gmail.com>
2018-03-07 15:53:52 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Short tut on MEAN stack
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/index.html')
+});
+
+
+app.post('/quotes', function (req,res) {
+    console.log(req.body)
+})
+
+app.listen(3000);
