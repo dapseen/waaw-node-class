3cdb22fd660e598bfda2f2c2fbbc6ace6e7f2efa
ajuwon adedapo <dapseen@gmail.com>
2018-03-08 18:01:45 +0100

    add mongo db module

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const MongoClient  = require('mongodb').MongoClient;
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+var db;
+
+MongoClient.connect('mongodb://dapseen:KeepM0ving@ds261678.mlab.com:61678/crud_app',function(err,database){
+    if(err) return console.log(err);
+    db = client.db('crud_app');
+    app.listen(3000,function(){
+        console.log('listening on 3000')
+    });
+});
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html')
 });
@@ -14,4 +23,3 @@ app.post('/quotes', function (req,res) {
     console.log(req.body)
 })
 
-app.listen(3000);
