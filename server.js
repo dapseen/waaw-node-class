9e38b800309a255ed55057ef3bbbad4548e9dcfa
ajuwon adedapo <dapseen@gmail.com>
2018-03-10 03:25:18 +0100

    working crud

--- server.js
+++ server.js
@@ -3,23 +3,43 @@ const bodyParser = require('body-parser');
 const MongoClient  = require('mongodb').MongoClient;
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(express.static('public'));
+//url
 
-var db;
+const url = 'mongodb://dapseen:KeepM0ving@ds261678.mlab.com:61678/crud_app';
 
-MongoClient.connect('mongodb://dapseen:KeepM0ving@ds261678.mlab.com:61678/crud_app',function(err,database){
+const dbname = 'crud_app';
+
+var db
+
+
+MongoClient.connect(url,function(err,client){
     if(err) return console.log(err);
-    db = client.db('crud_app');
+      db = client.db(dbname);
     app.listen(3000,function(){
         console.log('listening on 3000')
     });
 });
 app.get('/',function(req,res){
-    res.sendFile(__dirname + '/index.html')
+     db.collection('qoutes').find().toArray(function(err,result){
+        if(err) return console.log(err);
+        //render
+        res.render('index.ejs', {quotes : result});
+    });
+
 });
 
 
 app.post('/quotes', function (req,res) {
+   db.collection('qoutes').save(req.body,function(err,result){
+     if(err) return console.log(err);
+
+       console.log('record saved to database');
+       res.redirect('/');
+   });
     console.log(req.body)
-})
+});
 
